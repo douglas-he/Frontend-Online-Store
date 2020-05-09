8f832db03ad793b057eb70b1cafb5784252e154b
douglas-he <doughrqe@gmail.com>
2020-05-09 20:32:15 -0300

    Updates

---
+++ precache-manifest.76b9bba72c61aa6acba12c62b07c8576.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d714e5afbb97ace56c1a87d9fc84289e",
+    "url": "/Frontend-Online-Store/index.html"
+  },
+  {
+    "revision": "ba16ce120598f3b5d488",
+    "url": "/Frontend-Online-Store/static/css/main.c37a6b81.chunk.css"
+  },
+  {
+    "revision": "c3303d708d6391adf4fc",
+    "url": "/Frontend-Online-Store/static/js/2.fa49266d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Frontend-Online-Store/static/js/2.fa49266d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba16ce120598f3b5d488",
+    "url": "/Frontend-Online-Store/static/js/main.6d87a95a.chunk.js"
+  },
+  {
+    "revision": "cb03af2e79e3d6ed4790",
+    "url": "/Frontend-Online-Store/static/js/runtime-main.16c0c385.js"
+  },
+  {
+    "revision": "ee36df287adec84f2aa1caad0fbcb0fc",
+    "url": "/Frontend-Online-Store/static/media/empty_box.ee36df28.png"
+  },
+  {
+    "revision": "a0fb367ca52e42a7cf8f5cd9a8811241",
+    "url": "/Frontend-Online-Store/static/media/empty_car.a0fb367c.svg"
+  },
+  {
+    "revision": "32dd7d93285b8931c23327d030857424",
+    "url": "/Frontend-Online-Store/static/media/lupa.32dd7d93.png"
+  }
+]);
